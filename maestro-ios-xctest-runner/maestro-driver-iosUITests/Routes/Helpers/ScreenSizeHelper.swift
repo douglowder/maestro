3d4b21de6c5b6190132940d2bbd25e5271ca28d9
Max Phillips <max.phillips@nine.com.au>
2024-09-20 17:36:15 +1000

    update ScreenSizeHelper to compile on tvOS

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Helpers/ScreenSizeHelper.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Helpers/ScreenSizeHelper.swift
@@ -1,5 +1,16 @@
 import XCTest
 
+// UIKit doesn't include UIDeviceOrientation on tvOS
+public enum DeviceOrientation: Int, @unchecked Sendable {
+    case unknown = 0
+    case portrait = 1 // Device oriented vertically, home button on the bottom
+    case portraitUpsideDown = 2 // Device oriented vertically, home button on the top
+    case landscapeLeft = 3 // Device oriented horizontally, home button on the right
+    case landscapeRight = 4 // Device oriented horizontally, home button on the left
+    case faceUp = 5 // Device oriented flat, face up
+    case faceDown = 6 // Device oriented flat, face down
+}
+
 struct ScreenSizeHelper {
     
     private static var cachedSize: (Float, Float)?
@@ -7,7 +18,11 @@ struct ScreenSizeHelper {
     private static var lastOrientation: UIDeviceOrientation?
     
     static func physicalScreenSize() -> (Float, Float) {
+        #if os(tvOS)
+        let springboardBundleId = "com.apple.PineBoard"
+        #else
         let springboardBundleId = "com.apple.springboard"
+        #endif
         let app = RunningApp.getForegroundApp() ?? XCUIApplication(bundleIdentifier: springboardBundleId)
         let currentAppBundleId = app.bundleID
         let currentOrientation = XCUIDevice.shared.orientation
@@ -37,20 +52,25 @@ struct ScreenSizeHelper {
             return (Float(screenSize.width), Float(screenSize.height))
         }
     }
-    
-    private static func actualOrientation() -> UIDeviceOrientation {
-        let orientation = XCUIDevice.shared.orientation
-        if orientation == .unknown {
+
+    private static func actualOrientation() -> DeviceOrientation {
+        #if os(tvOS)
+        // Please don't rotate your AppleTV...
+        let orientation = Optional(DeviceOrientation.unknown)
+        #else
+        let orientation = DeviceOrientation(rawValue: XCUIDevice.shared.orientation.rawValue)
+        #endif
+
+        guard let unwrappedOrientation = orientation, orientation != .unknown else {
             // If orientation is "unknown", we assume it is "portrait" to
             // work around https://stackoverflow.com/q/78932288/7009800
-            return UIDeviceOrientation.portrait
+            return DeviceOrientation.portrait
         }
-        
-        return orientation
+        return unwrappedOrientation
     }
     
     /// Takes device orientation into account.
-    static func actualScreenSize() throws -> (Float, Float, UIDeviceOrientation) {
+    static func actualScreenSize() throws -> (Float, Float, DeviceOrientation) {
         let orientation = actualOrientation()
         
         let (width, height) = physicalScreenSize()
